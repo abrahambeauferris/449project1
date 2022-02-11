2f0bbf7b66b50cdf664963ef83e174e723be730b
abrahambeauferris <46635754+abrahambeauferris@users.noreply.github.com>
2022-02-10 23:04:57 -0700

    Fix misspelt class

--- Input.java
+++ Input.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Inout {
+public class Input {
 
   static ArrayList<ArrayList<Integer>> fp = new ArrayList<>();
   static ArrayList<ArrayList<Integer>> fm = new ArrayList<>();
